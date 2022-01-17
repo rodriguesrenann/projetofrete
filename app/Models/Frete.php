78d3e25de387656d05423a3ff4174c9d64d2875d
Renan Rodrigues <renanrodrigues_cb_@hotmail.com>
2022-01-16 21:24:37 -0300

    criando models e relacionamentos

---
+++ app/Models/Frete.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Frete extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = ['produto', 'endereco_entrega', 
+            'id_loja_vendedora', 'dia_frete', 'horario_frete', 'status_frete',
+            'levar_maquina','valor_frete', 'observacao', 'estoque_saida'];
+
+    public function loja()
+    {
+       return $this->belongsTo(Loja::class, 'id_loja_vendedora', 'id');
+    }
+}
